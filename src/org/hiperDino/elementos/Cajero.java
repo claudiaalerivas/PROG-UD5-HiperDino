0e7f20d0ec0ed32ee3c78e1cf32e41c15616f5aa
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:48:49 +0100

    Creacion metodo añadir cliente, y sobreescritura de motodo toString en clase Cajero

--- src/org/hiperDino/elementos/Cajero.java
+++ src/org/hiperDino/elementos/Cajero.java
@@ -3,6 +3,7 @@ package org.hiperDino.elementos;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import org.hiperDino.gestor.Gestor;
 
 public class Cajero {
   private static int id = 1;
@@ -22,7 +23,16 @@ public class Cajero {
     this.id = id;
   }
 
-
+  public static void aniadirCliente(String cliente){
+    if (Gestor.cajaAbierta) {
+      filaClientes.add(cliente);
+      contadorClientes++;
+      System.out.println("Cliente "+ cliente + " añadido a la cola");
+    } else {
+      System.out.println("Caja Cerrada ;( ");
+    }
+  }
+  
   public static void clienteAtendido(){
     System.out.println("Cliente Atendido: " + filaClientes.poll());
     contadorClientes--;
@@ -33,5 +43,11 @@ public class Cajero {
     System.out.println(filaClientes);
   }
 
+  @Override
+  public String toString() {
+    return "* Numero de caja: "+ id +"\n"+
+    "* Total Clientes: "+ contadorClientes +"\n"+
+    "* Clientes en la fila: "+ "\n" + filaClientes;
+  }
 
 }
