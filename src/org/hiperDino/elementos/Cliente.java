71d1c92ca2af910f245d48c1060b2377174438d1
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:13:38 +0100

    feat(elementos): Creacion de variables y metodos de acceso clase Cliente

--- src/org/hiperDino/elementos/Cliente.java
+++ src/org/hiperDino/elementos/Cliente.java
@@ -1,5 +1,16 @@
 package org.hiperDino.elementos;
 
 public class Cliente {
+  private String nombre; 
+  private Cesta cesta;
+  private int contadorArticulos = 0;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
   
 }
