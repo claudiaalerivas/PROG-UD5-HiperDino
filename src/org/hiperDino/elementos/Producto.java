aafdc4a54797c8dbffc0018ee91994c9110403a9
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:10:04 +0100

    feat(elementos): Creacion de variable, metodos de acceso y constructor de la clase producto

--- src/org/hiperDino/elementos/Producto.java
+++ src/org/hiperDino/elementos/Producto.java
@@ -1,5 +1,22 @@
 package org.hiperDino.elementos;
 
 public class Producto {
-  
+  private String nombre;
+
+  public Producto(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  @Override
+  public String toString() {
+    return nombre;
+  }
 }
