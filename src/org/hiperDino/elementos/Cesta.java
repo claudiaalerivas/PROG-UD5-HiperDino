43535d9134475df1e55e9c3f3f05153c114b5ddb
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:45:30 +0100

    feat(elementos): Creacion de metodos añadir productos aleatorios, eliminar producto y sobreescritura del metodo toString en la clase Cesta

--- src/org/hiperDino/elementos/Cesta.java
+++ src/org/hiperDino/elementos/Cesta.java
@@ -2,6 +2,8 @@ package org.hiperDino.elementos;
 
 import java.util.Stack;
 
+import org.hiperDino.utiles.Aleatorio;
+
 public class Cesta {
   private static Stack<Producto> pilaProductos = new Stack<>();
 
@@ -9,4 +11,24 @@ public class Cesta {
     this.pilaProductos = pilaProductos;
   }
 
+  public static void aniadirProducto(){
+    pilaProductos.add(Aleatorio.productoAleatorio());
+    System.out.println("Producto: "+ pilaProductos + " añadido");
+  }
+  public static void eliminarProducto(){
+    pilaProductos.pop();
+  }
+
+  @Override
+  public String toString() {
+    String producto = "";
+    for (Producto productos : this.pilaProductos) {
+      producto += productos;
+      producto += " \n ";
+      
+    }
+    return producto;
+  }
+
 }
+
