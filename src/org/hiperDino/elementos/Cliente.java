9fa4bdd30e51cfb05b75dc41604fcfe4103f3d45
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:43:20 +0100

    feat(elementos): Creacion de constructor cliente con implementacion de metodos aleatorios de cesta y cliente. creacion metodo contador productos, eliminar ultimo producto de la cesta, y sobreescritura del metodo toString

--- src/org/hiperDino/elementos/Cliente.java
+++ src/org/hiperDino/elementos/Cliente.java
@@ -1,9 +1,16 @@
 package org.hiperDino.elementos;
 
+import org.hiperDino.utiles.Aleatorio;
+
 public class Cliente {
   private String nombre; 
   private Cesta cesta;
   private int contadorArticulos = 0;
+
+  public Cliente() {
+    this.nombre = Aleatorio.clienteAletorio();
+    this.cesta = Aleatorio.cestaAleatoria();
+  }
 
   public String getNombre() {
     return nombre;
@@ -12,5 +19,20 @@ public class Cliente {
   public void setNombre(String nombre) {
     this.nombre = nombre;
   }
+  public void contadorProductos(){
+    if (!cesta.equals(null)) {
+      contadorArticulos++;
+    }
+  }
+  public void eliminarUltimoProductoCesta(Producto producto){
+    Cesta.eliminarProducto();
+  }
   
+  @Override
+  public String toString() {
+    return "* Nombre:" + nombre +"\n"+ 
+    "* Total de Productos: " + contadorArticulos + "\n" +
+    "* Lista articulos en la cesta :" + "\n" + cesta ;
+  }
 }
+
