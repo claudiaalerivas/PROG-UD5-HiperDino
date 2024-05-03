f5ab39f76636321ba9c43c0895650c2f6f8a1a8e
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:56:36 +0100

    feat(menu): creacion de menu y ejecutando el metodo mostrarMenu en la clase ejecutable App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import org.hiperDino.menu.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu menu = new Menu();
+        menu.mostrarMenu();
     }
 }
