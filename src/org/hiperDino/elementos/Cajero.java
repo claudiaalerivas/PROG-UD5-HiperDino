eb0859eacc3fb0179cc52310873b687d58900952
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-05-03 13:28:03 +0100

    feat(elementos): Creacion de variables, fila de clientes, metodos de acceso,  metodos cliente atendido, mostrar clientes

--- src/org/hiperDino/elementos/Cajero.java
+++ src/org/hiperDino/elementos/Cajero.java
@@ -1,1 +1,43 @@
+package org.hiperDino.elementos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Cajero {
+  private static int id = 1;
+  public static Queue<String> filaClientes = new LinkedList<>();
+  public static int contadorClientes = 0;
+  
+  
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public Cajero(int id) {
+    this.id = id;
+  }
+
+
+  public static void clienteAtendido(){
+    System.out.println("Cliente Atendido: " + filaClientes.poll());
+    contadorClientes--;
+  }
+
+  public static void mostrarClientes(){
+    System.out.println("Caja numero: " + id);
+    System.out.println(filaClientes);
+  }
+
+  @Override
+  public String toString() {
+    return "* Numero de caja: "+ id +"\n"+
+    "* Total Clientes: "+ contadorClientes +"\n"+
+    "* Clientes en la fila: "+ "\n" + filaClientes;
+  }
+
+}
--- src/org/hiperDino/elementos/Cesta.java
+++ src/org/hiperDino/elementos/Cesta.java
@@ -1,5 +1,12 @@
 package org.hiperDino.elementos;
 
+import java.util.Stack;
+
 public class Cesta {
-  
+  private static Stack<Producto> pilaProductos = new Stack<>();
+
+  public Cesta(Stack<Producto> pilaProductos) {
+    this.pilaProductos = pilaProductos;
+  }
+
 }
